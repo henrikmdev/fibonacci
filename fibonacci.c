e2cc131b6092083d5b8590f6f24cd835f966dfcb
henrikmdev <henrikmdev@gmail.com>
2023-07-04 22:27:37 -0400

    Update code comments

--- fibonacci.c
+++ fibonacci.c
@@ -4,33 +4,41 @@ int fib(int n);
 
 int main(void) {
     int returnValue = 0;
-    printf("Fibonacci coding challenge\n");
+    printf("\n\nFibonacci coding challenge\n\n");
     returnValue = fib(9);
-    printf("The number is %d", returnValue);
+    printf("The number is %d\n\n", returnValue);
 }
 
+/*****************************************************************
+ * fib
+ * calculates the nth number of the fibonacci sequence
+ * 
+ * arguments:
+ *     int n - the desired number of the fibonacci sequence
+ * 
+ * return:
+ *     int - the calculated nth number of the fibonacci sequence
+ *****************************************************************/
 int fib(int n) {
-    //printf("I am in fib\n");
-    if (n==0) {
+    /* alternate solution for fib */
+    /*if (n==0) {
         return 0;
     }
     if (n==1) {
         return 1;
     }
-    // n > 1 case Fn-1, Fn-2??
-    // pretend 4, F3 and F2, F4=F3+F2, F3=F2+F1, F2=F1+F0
-    // pretend 2, F2=F1+F0
-    // n=2, fib(2-2), fib(2-1)
-    return fib(n-2)+fib(n-1);
+    return fib(n-2)+fib(n-1);*/
 
-    int lastTwo[2]={0,1};
-    int nthNumber = 0;
+    int lastTwo[2]={0,1}; /* the last two numbers */
+    int nthNumber = 0; /* this is the number that is returned */
     int numIterations = 0;
     while (numIterations < n-1) {
-        nthNumber = lastTwo[0]+lastTwo[1]; //F2
-        printf("%d + %d = %d\n", lastTwo[0], lastTwo[1], nthNumber);
-        lastTwo[0] = lastTwo[1]; //replaces F0 with F1
-        lastTwo[1] = nthNumber; //replaces F1 with F2
+        nthNumber = lastTwo[0]+lastTwo[1]; /* Fn - where the next number is calculated */
+        //printf("%d + %d = %d\n", lastTwo[0], lastTwo[1], nthNumber);
+        
+        /* updating lastTwo for the next iteration */
+        lastTwo[0] = lastTwo[1]; /* replaces Fn-2 with Fn-1 */
+        lastTwo[1] = nthNumber; /* replaces Fn-1 with Fn */
         numIterations++;
     }
     return nthNumber;
